2bebb3ebc6e3ec8a293d9d11f6204ae166618031
Ryan William Tierney <ryan.tierney92@gmail.com>
2019-09-07 13:46:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,25 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["Guitar", "Drums", "Bass", "Keys"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  add_element_to_end_of_array = ["wow", "I", "am", "really", "learning"]
+ add_element_to_end_of_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+ add_element_to_start_of_array = ["I", "am", "really", "learning"]
+ add_element_to_start_of_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  remove_element_from_end_of_array ["I", "am", "really", "learning", "arrays!"]
+  remove_element = "arrays!"
+  remove_element = remove_element_from_end_of_array.pop
 end
 
 def remove_element_from_start_of_array(array)
